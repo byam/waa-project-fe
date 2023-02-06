b4eeb941e26517d0621250c950fa4f2d6ee51a05
Ismoil Shokirov <ismoil.793@gmail.com>
2023-02-06 15:38:13 -0600

    created notification and api services

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { increment, decrement } from './store/slices/counter';
+import { notifyError, notifySuccess } from './helpers/notification';
 
 function App() {
   const counter = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
+
+  const handleAddClick = () => {
+    dispatch(increment());
+    notifySuccess('Clicked increment');
+  };
+
+  const handleDecrementClick = () => {
+    dispatch(decrement());
+    notifyError('Clicked decrement');
+  };
 
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello WAA!</h1>
 
       <div>Counter: {counter}</div>
-      <button onClick={() => dispatch(increment())} type="button">
+      <button onClick={handleAddClick} type="button">
         Plus
       </button>
-      <button onClick={() => dispatch(decrement())} type="button">
+      <button onClick={handleDecrementClick} type="button">
         Minus
       </button>
     </div>
