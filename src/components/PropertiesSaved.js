24ae4770391d0f258305d21bfeec767d5f40e967
bya <byambaa.mng@gmail.com>
2023-02-07 17:34:24 -0600

    fix

--- src/components/PropertiesSaved.js
+++ src/components/PropertiesSaved.js
@@ -40,23 +40,22 @@ function PropertiesSaved() {
     <div className="bg-white">
       <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {currentUser?.role === 'customer' &&
-            properties.map((p) => (
-              <div key={p.id}>
-                <Link key={p.id} to={`/properties/${p.id}`} className="group">
-                  <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-                    <img
-                      src={p.imageSrc}
-                      alt={p.description}
-                      className="h-full w-full object-cover object-center group-hover:opacity-75"
-                    />
-                  </div>
-                  <h3 className="mt-4 text-sm text-gray-700">{p.title}</h3>
-                  <p className="mt-1 text-lg font-medium text-gray-900">${p.price}</p>
-                </Link>
-                {heartIcon(p)}
-              </div>
-            ))}
+          {properties.map((p) => (
+            <div key={p.id}>
+              <Link key={p.id} to={`/properties/${p.id}`} className="group">
+                <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+                  <img
+                    src={p.imageSrc}
+                    alt={p.description}
+                    className="h-full w-full object-cover object-center group-hover:opacity-75"
+                  />
+                </div>
+                <h3 className="mt-4 text-sm text-gray-700">{p.title}</h3>
+                <p className="mt-1 text-lg font-medium text-gray-900">${p.price}</p>
+              </Link>
+              {heartIcon(p)}
+            </div>
+          ))}
         </div>
       </div>
     </div>
