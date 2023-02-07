f7baddee3bca0ec74b718c54e2b2090642e54974
Ismoil Shokirov <ismoil.793@gmail.com>
2023-02-07 15:56:47 -0600

    stateful login logout

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ Make sure you are using node "node" : "16.13.2" or higher
 
 ### `npm install`
 
-
 ### `npm start`
 
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,49 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { useDispatch } from 'react-redux';
+import { notifyError, notifySuccess } from '../helpers/notification';
+import { signUp } from '../store/slices/auth';
+
 function SignUp() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    country: 'United States',
+    street: '',
+    city: '',
+    region: '',
+    zipCode: '',
+  });
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleSignupInputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSignupSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      console.log(formData);
+      // await TODO: signup API call
+      dispatch(signUp(formData));
+      notifySuccess('Successfully signed up');
+      // replace: true means Browser navigation history will be cleared
+      navigate('/', { replace: true });
+    } catch (err) {
+      notifyError(`Error while signing up ${err}`);
+    }
+  };
+
   return (
     <div className="flex items-center justify-center py-16">
       <div className="md:grid-cols-3 md:gap-6">
         <div className="mt-5 md:col-span-2 md:mt-0">
-          <form action="#" method="POST">
+          <form onSubmit={handleSignupSubmit}>
             <div className="overflow-hidden shadow sm:rounded-md">
               <div className="bg-white px-4 py-5 sm:p-6">
                 <div className="grid grid-cols-6 gap-6">
@@ -11,9 +51,9 @@ function SignUp() {
                     First name
                     <input
                       type="text"
-                      name="first-name"
-                      id="first-name"
-                      autoComplete="given-name"
+                      name="firstName"
+                      onChange={handleSignupInputChange}
+                      value={formData.firstName}
                       className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                     />
                   </div>
@@ -22,9 +62,9 @@ function SignUp() {
                     Last name
                     <input
                       type="text"
-                      name="last-name"
-                      id="last-name"
-                      autoComplete="family-name"
+                      name="lastName"
+                      onChange={handleSignupInputChange}
+                      value={formData.lastName}
                       className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                     />
                   </div>
@@ -33,9 +73,9 @@ function SignUp() {
                     Email address
                     <input
                       type="text"
-                      name="email-address"
-                      id="email-address"
-                      autoComplete="email"
+                      name="email"
+                      onChange={handleSignupInputChange}
+                      value={formData.email}
                       className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                     />
                   </div>
@@ -43,9 +83,9 @@ function SignUp() {
                   <div className="col-span-6 sm:col-span-3">
                     Country
                     <select
-                      id="country"
                       name="country"
-                      autoComplete="country-name"
+                      onChange={handleSignupInputChange}
+                      value={formData.country}
                       className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                     >
                       <option>United States</option>
@@ -58,9 +98,9 @@ function SignUp() {
                     Street address
                     <input
                       type="text"
-                      name="street-address"
-                      id="street-address"
-                      autoComplete="street-address"
+                      name="street"
+                      onChange={handleSignupInputChange}
+                      value={formData.street}
                       className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                     />
                   </div>
@@ -70,8 +110,8 @@ function SignUp() {
                     <input
                       type="text"
                       name="city"
-                      id="city"
-                      autoComplete="address-level2"
+                      onChange={handleSignupInputChange}
+                      value={formData.city}
                       className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                     />
                   </div>
@@ -81,8 +121,8 @@ function SignUp() {
                     <input
                       type="text"
                       name="region"
-                      id="region"
-                      autoComplete="address-level1"
+                      onChange={handleSignupInputChange}
+                      value={formData.region}
                       className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                     />
                   </div>
@@ -91,9 +131,9 @@ function SignUp() {
                     ZIP / Postal code
                     <input
                       type="text"
-                      name="postal-code"
-                      id="postal-code"
-                      autoComplete="postal-code"
+                      name="zipCode"
+                      onChange={handleSignupInputChange}
+                      value={formData.zipCode}
                       className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                     />
                   </div>
