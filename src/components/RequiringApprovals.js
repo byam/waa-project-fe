c3c007a78aaaa583b4aac5e694552514f2cffe94
bya <byambaa.mng@gmail.com>
2023-02-08 16:18:31 -0600

    fix

--- src/components/RequiringApprovals.js
+++ src/components/RequiringApprovals.js
@@ -1,3 +1,4 @@
+/* eslint-disable object-curly-newline */
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Table, Space, Button, Modal } from 'antd';
